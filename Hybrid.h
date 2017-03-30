eab31404f240ea2d7c21da4077b859d592ab3634
Elias Thomas Brady <etbrady42@students.tntech.edu>
2017-03-30 12:46:58 -0500

    first commit

---
+++ Hybrid.h
@@ -0,0 +1,75 @@
+#if !defined (HYBRID_H)
+#define HYBRID_H
+
+#include "QueueLinked.h"
+using CSC2110::QueueLinked;
+#include "SortedListDoublyLinked.h"
+
+template < class T >
+class Hybrid
+{
+
+   private:
+      QueueLinked<T>* q;
+      SortedListDoublyLinked<T>* sldl;
+
+   public:
+      Hybrid(int (*comp_items) (T* item_1, T* item_2), int (*comp_keys) (String* key, T* item));
+      ~Hybrid();
+
+      bool isEmpty();
+      void enqueue(T* item);
+      T* dequeue();
+      ListDoublyLinkedIterator<T>* iterator();
+
+};
+
+template < class T >
+Hybrid<T>::Hybrid(int (*comp_items) (T* item_1, T* item_2), int (*comp_keys) (String* key, T* item))
+{
+   q = new QueueLinked<T>();
+   sldl = new SortedListDoublyLinked<T>(comp_items, comp_keys);
+}
+
+template < class T >
+Hybrid<T>::~Hybrid()
+{
+   delete q;
+   delete sldl;
+}
+
+//DO THIS
+//complete the implementation for the Hybrid ADT in two different ways
+//as outlined in the Lab 10 description
+//simply comment the first implementation out when working on the second implementation
+//use the getKey method to dequeue/remove
+
+template < class T >
+bool Hybrid<T>::isEmpty()
+{
+	return ((q->size() == 0) && (sldl->size() == 0));
+}
+template < class T >
+void Hybrid<T>::enqueue(T* item)
+{
+	q->enqueue(item);
+	sldl->add(item);
+}
+template < class T >
+T* Hybrid<T>::dequeue()
+{
+	T* item = q->dequeue();
+	sldl->remove(item->getKey());
+	return item;
+}
+template < class T >
+ListDoublyLinkedIterator<T>* Hybrid<T>::iterator()
+{
+	ListDoublyLinkedIterator<T>* iter = sldl->iterator();
+	return iter;
+}
+
+
+
+
+#endif
